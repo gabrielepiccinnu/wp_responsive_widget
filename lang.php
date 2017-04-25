a31c751797c3b4e5cbb5d41a980900af1aa3b00e
Николай | dfan <devdfan@gmail.com>
2017-04-25 18:21:12 +0200

    Fix patch

--- lang.php
+++ lang.php
@@ -6,15 +6,15 @@ function wubook_or_init() {
 		unset( $_COOKIE[ 'wb_lang' ] );
 		$lang = $_POST[ 'wb_lang' ];
 		setcookie( "wb_lang", $lang, time() + ( 86400 * 30 ), "/" );
-		load_textdomain( 'wubook_or', WP_PLUGIN_DIR . '/wp_responsive_widget/lang/wubook_or-' . $lang . '.mo' );
+		load_textdomain( 'wubook_or', WP_PLUGIN_DIR . '/wp_responsive_widget-master/lang/wubook_or-' . $lang . '.mo' );
 	} else {
 
 		$lang = $_COOKIE[ 'wb_lang' ];
 
 		if ( $lang == '' ) {
-			load_textdomain( 'wubook_or', WP_PLUGIN_DIR . '/wp_responsive_widget/lang/wubook_or-en_US.mo' );
+			load_textdomain( 'wubook_or', WP_PLUGIN_DIR . '/wp_responsive_widget-master/lang/wubook_or-en_US.mo' );
 		} else {
-			load_textdomain( 'wubook_or', WP_PLUGIN_DIR . '/wp_responsive_widget/lang/wubook_or-' . $lang . '.mo' );
+			load_textdomain( 'wubook_or', WP_PLUGIN_DIR . '/wp_responsive_widget-master/lang/wubook_or-' . $lang . '.mo' );
 		}
 
 	}
--- main.php
+++ main.php
@@ -11,7 +11,7 @@
 
 if ( !defined( "WPINC" ) ) { die( "WuBook" ); }
 
-function wb_or_($file){ require_once WP_PLUGIN_DIR . '/wp_responsive_widget/' . $file; }
+function wb_or_($file){ require_once WP_PLUGIN_DIR . '/wp_responsive_widget-master/' . $file; }
 
 add_action( 'plugins_loaded', 'wubook_or_init' );
 add_action( 'admin_menu', 'wb_or_menu' );
