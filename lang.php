addd0d9e3c3dbc4c0b728d7c56c8870402adab39
Gabriele Piccinnu <gabrielepiccinnu@gmail.com>
2025-02-01 16:32:35 +0100

    $wb_lang fix check

--- lang.php
+++ lang.php
@@ -2,24 +2,20 @@
 
 function wubook_or_init() {
 
-	if ( $_POST[ 'wb_lang' ] ) {
-		unset( $_COOKIE[ 'wb_lang' ] );
-		$lang = $_POST[ 'wb_lang' ];
-		setcookie( "wb_lang", $lang, time() + ( 86400 * 30 ), "/" );
-		load_textdomain( 'wubook_or', WP_PLUGIN_DIR . '/wp_responsive_widget-master/lang/wubook_or-' . $lang . '.mo' );
-	} else {
+    // Controlla se il valore esiste in $_POST prima di utilizzarlo
+    if (isset($_POST['wb_lang']) && !empty($_POST['wb_lang'])) {
+        unset($_COOKIE['wb_lang']);
+        $lang = $_POST['wb_lang'];
+        setcookie("wb_lang", $lang, time() + (86400 * 30), "/");
+        load_textdomain('wubook_or', WP_PLUGIN_DIR . '/wp_responsive_widget-master/lang/wubook_or-' . $lang . '.mo');
+    } else {
+        // Controlla se il valore esiste in $_COOKIE prima di utilizzarlo
+        $lang = isset($_COOKIE['wb_lang']) ? $_COOKIE['wb_lang'] : '';
 
-		$lang = $_COOKIE[ 'wb_lang' ];
-
-		if ( $lang == '' ) {
-			load_textdomain( 'wubook_or', WP_PLUGIN_DIR . '/wp_responsive_widget-master/lang/wubook_or-en_US.mo' );
-		} else {
-			load_textdomain( 'wubook_or', WP_PLUGIN_DIR . '/wp_responsive_widget-master/lang/wubook_or-' . $lang . '.mo' );
-		}
-
-	}
-
+        if ($lang == '') {
+            load_textdomain('wubook_or', WP_PLUGIN_DIR . '/wp_responsive_widget-master/lang/wubook_or-en_US.mo');
+        } else {
+            load_textdomain('wubook_or', WP_PLUGIN_DIR . '/wp_responsive_widget-master/lang/wubook_or-' . $lang . '.mo');
+        }
+    }
 }
-
-
-?>
